8587833a0364255914d8400e41623661223c36fe
hammad <engineerraohammadali@gmail.com>
2025-10-22 15:53:46 +0500

    move to start

--- src/main/java/com/springadmin/portal/core/model/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springadmin.portal.core.model;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-
-}
